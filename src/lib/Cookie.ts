1aed65190663cbe79531901a353fc4453286d6d2
Tim Bowersox <11789189+t-bowersox@users.noreply.github.com>
2022-01-31 17:17:49 -0500

    docs: add docs to Cookie methods

--- src/lib/Cookie.ts
+++ src/lib/Cookie.ts
@@ -47,36 +47,72 @@ export class Cookie {
 
   constructor(private _name: string, private _value: string) {}
 
+  /**
+   * Sets the cookie's name.
+   * @param name
+   * @returns The `Cookie` instance.
+   */
   setName(name: string): Cookie {
     this._name = name;
     return this;
   }
 
+  /**
+   * Sets the cookie's value.
+   * @param value
+   * @returns The `Cookie` instance.
+   */
   setValue(value: string): Cookie {
     this._value = value;
     return this;
   }
 
+  /**
+   * Sets the cookie's `Expires` attribute.
+   * If provided a `Date` object, it will be converted to a UTC string.
+   * @param date A `Date` object or a UTC date string.
+   * @returns The `Cookie` instance.
+   */
   setExpires(date: Date | string): Cookie {
     this._expires = date instanceof Date ? date.toUTCString() : date;
     return this;
   }
 
+  /**
+   * Sets the cookie's `Max-Age` attribute.
+   * @param maxAge The maximum age, in seconds.
+   * @returns The `Cookie` instance.
+   */
   setMaxAge(maxAge: number): Cookie {
     this._maxAge = maxAge;
     return this;
   }
 
+  /**
+   * Sets the cookie's `Domain` attribute.
+   * @param domain
+   * @returns The `Cookie` instance.
+   */
   setDomain(domain: string): Cookie {
     this._domain = domain;
     return this;
   }
 
+  /**
+   * Sets the cookie's `Path` attribute.
+   * @param path
+   * @returns The `Cookie` instance.
+   */
   setPath(path: string): Cookie {
     this._path = path;
     return this;
   }
 
+  /**
+   * Sets the cookie's `Secure` attribute.
+   * @param secure `true` to include the `Secure` attribute; `false` to exclude it. Each new `Cookie` instance defaults to `true`.
+   * @returns The `Cookie` instance.
+   */
   setSecure(secure: boolean): Cookie {
     if (this._sameSite === SameSite.None && !secure) {
       throw new CookieError(
@@ -87,11 +123,22 @@ export class Cookie {
     return this;
   }
 
+  /**
+   * Sets the cookie's `HttpOnly` attribute.
+   * @param httpOnly `true` to include the `HttpOnly` attribute; `false` to exclude it. Each new `Cookie` instance defaults to `true`.
+   * @returns The `Cookie` instance.
+   */
   setHttpOnly(httpOnly: boolean): Cookie {
     this._httpOnly = httpOnly;
     return this;
   }
 
+  /**
+   * Sets the cookie's `SameSite` attribute.
+   * @param sameSite One of "Strict", "Lax", or "None".
+   * If set to "None", `Secure` will be set to `true` because such cookies require a secure context.
+   * @returns The `Cookie` instance.
+   */
   setSameSite(sameSite: SameSite): Cookie {
     this._sameSite = sameSite;
     if (sameSite === SameSite.None) {
@@ -101,6 +148,10 @@ export class Cookie {
     return this;
   }
 
+  /**
+   * Returns the `Cookie` instance as a string ready to use in a `Set-Cookie` header.
+   * @returns The `Cookie` as a string.
+   */
   toString(): string {
     let cookie = `${this.name}=${this.value}; SameSite=${this.sameSite}`;
 
@@ -114,6 +165,15 @@ export class Cookie {
     return cookie;
   }
 
+  /**
+   * Parses a cookie string into a new `Cookie` instance.
+   * When using this method, `Secure` and `HttpOnly` are _not_ enabled unless they
+   * are enabled in the cookie string. In keeping with browser behavior, if the string
+   * does not include a `SameSite` attribute, a default of "Lax" will be applied to the
+   * new `Cookie` instance.
+   * @param cookieStr A cookie string from a `Set-Cookie` header.
+   * @returns A new `Cookie` instance.
+   */
   static parse(cookieStr: string): Cookie {
     const cookieArr = cookieStr.split("; ").map((attr) => attr.split("="));
     const cookieObj: RawCookie = Object.fromEntries(cookieArr);
